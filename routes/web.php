2577ed1b446dc82d4d391eaba518faa2c704d25e
jjrohrer <jj.bitbucket@rohrer.org>
2020-09-18 09:00:08 -0400

    grab route from tas-ui-looks

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,15 @@ require_once(base_path('vendor/eleganttechnologies/grok/src/routes/web.php'));
 
 Route::get('/grok/grok',function () {return view('grok::groks/grok/index');})->name('grok.grok');
 Route::get('/grok/livewire',function () {return view('grok::groks/livewire/index');})->name('grok.livewire');
-Route::get('/grok/tas-ui-looks',function () {return view('tas::groks/index');})->name('grok.tas-ui-looks');
 Route::get('/grok/jetstream',function () {return view('grok::groks/jetstream/index');})->name('grok.jetstream');
 
 
 Route::get('/grok',function () {return view('grok::index');})->name('grok');
 Route::get('/grok',function () {return view('grok::index');})->name('grok');
+
+require_once(__DIR__."/../../../TallAndSassy/TasUiLooks/routes/web.php");
+require_once(__DIR__."/../../../TallAndSassy/TasUiGlances/routes/web.php");
+
 
 //Route::middleware(['auth:sanctum', 'verified'])->get('/grok', function () {
 //   return 'hello world';#view('grok::index');
